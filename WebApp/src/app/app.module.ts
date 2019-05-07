b37f09cd8eb4e6d0d2196324e09a49eeb223d298
Cris_B <35538886+cristianmtb@users.noreply.github.com>
2019-05-07 18:29:55 +0200

    Added login and singup pages

--- WebApp/src/app/app.module.ts
+++ WebApp/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './components/app.component';
 import { WelcomePageComponent } from './components/welcome-page/welcome-page.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { SingupPageComponent } from './components/singup-page/singup-page.component';
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomePageComponent  ],
+    WelcomePageComponent,
+    LoginPageComponent,
+    SingupPageComponent  ],
   imports: [
     BrowserModule,
     AppRoutingModule
