1a047a2e1d44f82c03d6392b8b776dd69f437e11
Ahmad AR <38329562+Ahmadar91@users.noreply.github.com>
2019-05-09 18:59:42 +0200

    Added Bootstrap MD

--- WebApp/src/app/app.module.ts
+++ WebApp/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './components/app.component';
 import { WelcomePageComponent } from './components/welcome-page/welcome-page.component';
 import { LoginPageComponent } from './components/login-page/login-page.component';
 import { SingupPageComponent } from './components/singup-page/singup-page.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 
 
@@ -15,10 +17,12 @@ import { HomePageComponent } from './components/home-page/home-page.component';
     WelcomePageComponent,
     LoginPageComponent,
     SingupPageComponent,
-    HomePageComponent  ],
+    HomePageComponent,
+    NavBarComponent  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MDBBootstrapModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
