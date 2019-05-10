05171593a395aacc82db3a3914b725018b619d0c
Cris_B <35538886+cristianmtb@users.noreply.github.com>
2019-05-11 00:57:01 +0200

    Improvements on navigation and authentication

--- WebApp/src/app/modules/coordinator/coordinator.module.ts
+++ WebApp/src/app/modules/coordinator/coordinator.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CoordinatorRoutingModule } from './coordinator-routing.module';
-import { RootComponent } from './components/root/root.component';
+import { RootComponent } from './components/root.component';
 
 @NgModule({
   declarations: [
