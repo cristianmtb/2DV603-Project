4c28fd2a5a2589e2380df3e72faf0477fbc5d34b
Cris_B <35538886+cristianmtb@users.noreply.github.com>
2019-05-10 18:02:01 +0200

    Updated auth with role check and 2 demo "accounts"

--- WebApp/src/app/services/auth/auth.service.ts
+++ WebApp/src/app/services/auth/auth.service.ts
@@ -36,15 +36,51 @@ export class AuthService {
 
   login(username: string, password: string)
   {
+    if(username === 'coord' && password === 'coord') 
+    {
+      this.currentUser = new User;
+      this.currentUser.password = 'coord';
+      this.currentUser.username = 'coord';
+      this.currentUser.coordinator = true;
+      this.currentUser.supervisor = true;
+      return true;
+    }
     if(username === 'admin' && password === 'admin') 
     {
       this.currentUser = new User;
       this.currentUser.password = 'admin';
       this.currentUser.username = 'admin';
-      this.currentUser.role_id = 1;
+      this.currentUser.reader = true;
+      this.currentUser.student = true;
+      this.currentUser.opponent = true;
       return true;
     }
     return false;
      //return this.http.post<any>(`${config.apiUrl}/users/authenticate`, { username, password })
+  }
+  isCoordinator()
+  {
+    if(this.isLoggedIn() && this.currentUser.coordinator === true) return true;
+    return false;
+  }
+  isStudent()
+  {
+    if(this.isLoggedIn() && this.currentUser.student === true) return true;
+    return false;
+  }
+  isSupervisor()
+  {
+    if(this.isLoggedIn() && this.currentUser.supervisor === true) return true;
+    return false;
+  }
+  isReader()
+  {
+    if(this.isLoggedIn() && this.currentUser.reader === true) return true;
+    return false;
+  }
+  isOpponent()
+  {
+    if(this.isLoggedIn() && this.currentUser.opponent === true) return true;
+    return false;
   }
 }
