9f34125a4016a5aa9735930b718a98097a51819e
Cris_B <35538886+cristianmtb@users.noreply.github.com>
2019-05-13 13:26:55 +0200

    small updates #1

--- WebApp/src/app/models/user.ts
+++ WebApp/src/app/models/user.ts
@@ -2,9 +2,12 @@ export class User
 {
     username: string;
     password: string;
+    roleID: number;
+    //role check
     coordinator: boolean;  
     student: boolean;
     supervisor: boolean;
     reader: boolean;
     opponent: boolean;
+    //
 }
