4d8bf0cca98957df0912ce98d21defa0f2268b41
Cris_B <35538886+cristianmtb@users.noreply.github.com>
2019-05-21 17:15:47 +0200

    student module routing

--- WebApp/src/app/modules/student/components/root/root.component.ts
+++ WebApp/src/app/modules/student/components/root/root.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'root',
   templateUrl: './root.component.html',
   styleUrls: ['./root.component.scss']
 })
