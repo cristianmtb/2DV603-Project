dc13dc432c1e428b43319d8f6bafbe643df9a811
Cris_B <35538886+cristianmtb@users.noreply.github.com>
2019-05-26 13:55:07 +0200

    added submit component for student

---
+++ WebApp/src/app/modules/student/components/submit-document/submit-document.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-submit-document',
+  templateUrl: './submit-document.component.html',
+  styleUrls: ['./submit-document.component.scss']
+})
+export class SubmitDocumentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- WebApp/src/app/modules/student/student.module.ts
+++ WebApp/src/app/modules/student/student.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { StudentRoutingModule } from './student-routing.module';
 import { RootComponent } from './components/root/root.component';
+import { SubmitDocumentComponent } from './components/submit-document/submit-document.component';
 
 @NgModule({
-  declarations: [RootComponent],
+  declarations: [RootComponent, SubmitDocumentComponent],
   imports: [
     CommonModule,
     StudentRoutingModule
