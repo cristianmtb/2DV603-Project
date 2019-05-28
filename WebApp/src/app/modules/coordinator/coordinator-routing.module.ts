6c9e337f04bbedb8a8ef90679d477229c01744ef
Ahmad AR <38329562+Ahmadar91@users.noreply.github.com>
2019-05-29 01:35:30 +0200

    Merge branch 'fixes' of https://github.com/cristianmtb/2DV603-Project into fixes
    # Conflicts:
    #	WebApp/src/config.json

--- WebApp/src/app/modules/coordinator/coordinator-routing.module.ts
+++ WebApp/src/app/modules/coordinator/coordinator-routing.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CoordinatorComponent } from './coordinator.component';
 import { UsersComponent } from './components/users/users.component';
+import {PlansComponent} from "./components/plans/plans.component";
+import {DescriptionsComponent} from "./components/descriptions/descriptions.component";
+import {FinalReportsComponent} from "./components/final-reports/final-reports.component";
 
 
 const routes: Routes = [
@@ -13,7 +16,19 @@ const routes: Routes = [
   {
     path: 'users',
     component: UsersComponent
-  }
+  },
+  {
+    path: 'descriptions',
+    component: DescriptionsComponent
+  },
+  {
+    path: 'plans',
+    component: PlansComponent
+  },
+  {
+    path: 'finals',
+    component: FinalReportsComponent
+  },
 ];
 
 @NgModule({
--- WebApp/src/app/modules/coordinator/coordinator.module.ts
+++ WebApp/src/app/modules/coordinator/coordinator.module.ts
@@ -9,6 +9,9 @@ import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {AddComponent} from "./components/users/components/add/add.component";
 import {SharedModule} from "../../shared.module";
 import {EditComponent} from "./components/users/components/edit/edit.component";
+import { DescriptionsComponent } from './components/descriptions/descriptions.component';
+import { PlansComponent } from './components/plans/plans.component';
+import { FinalReportsComponent } from './components/final-reports/final-reports.component';
 
 
 
@@ -18,6 +21,9 @@ import {EditComponent} from "./components/users/components/edit/edit.component";
     UsersComponent,
     AddComponent,
     EditComponent,
+    DescriptionsComponent,
+    PlansComponent,
+    FinalReportsComponent,
 
   ],
   imports: [
